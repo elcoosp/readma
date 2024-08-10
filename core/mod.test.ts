ea400672424ca1e237b35d4799eb79c31ce5d94e
elcoosp <elcoosp@gmail.com>
2024-08-11 00:45:34 +0200

    chore(deno): test fix

--- core/mod.test.ts
+++ core/mod.test.ts
@@ -3,7 +3,7 @@ import { readme } from "./mod.ts";
 import config from "../scripts/readma.config.ts";
 
 Deno.test("readme", async () => {
-  const current = await Deno.readTextFile("./README.md");
+  const current = await Deno.readTextFile("../README.md");
   const out = await readme(config, { folderPath: "./" });
   assertEquals(out, current);
 });

7c078c4e2fd7869c14828eb59c56731e243b3b44
elcoosp <elcoosp@gmail.com>
2024-08-11 01:23:19 +0200

    ci(core): fix test folderPath

--- core/mod.test.ts
+++ core/mod.test.ts
@@ -4,6 +4,6 @@ import config from "../scripts/readma.config.ts";
 
 Deno.test("readme", async () => {
   const current = await Deno.readTextFile("../README.md");
-  const out = await readme(config, { folderPath: "./" });
+  const out = await readme(config, { folderPath: "../" });
   assertEquals(out, current);
 });

900a9186cd2e08bc96745f149c9fbcc303358de0
elcoosp <elcoosp@gmail.com>
2024-08-10 22:10:39 +0200

    chore(scripts): example for own readme

---
+++ scripts/gen-readme.ts
@@ -0,0 +1,34 @@
+import { readme } from "../mod.ts";
+import { md } from "../utils.ts";
+const name = "readma";
+const author = "elcoosp";
+readme({
+  language: "ts",
+  title: "Readma",
+  author,
+  github_username: author,
+  repo_name: name,
+  x_handle: author,
+  domain: "gmail",
+  email: author,
+  images: { screenshot: "images/screenshot.gif", logo: "images/logo.png" },
+  sections: {
+    project_description: "Beautiful effortless README generator",
+    about: "Create README that rocks",
+    installation: md.code(`deno install ${name}`),
+    acknowledgments: `
+- [Best Readme](https://github.com/othneildrew/Best-README-Template)
+- [Table of content library](https://github.com/a-ahandani/ah-toc)`,
+    getting_started: "See screenshot",
+    roadmap: `
+- [x] Logo design
+- [ ] CLI with config file
+- [ ] Auto defaults from known files like Cargo.toml/{deno,package}.json`,
+    usage: "See `scripts/gen-readme.ts`",
+  },
+  template: {
+    bug_report: "bug-report--from-readme",
+    feat_request: "feature-request---from-readme",
+  },
+  back_to_top: false,
+}, { folderPath: "./" });

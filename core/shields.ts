2ed116398939038eaa7a1317f4ce52c9cccfc177
elcoosp <elcoosp@gmail.com>
2024-11-20 17:58:53 +0100

    feat: add basic gh shields

--- core/shields.ts
+++ core/shields.ts
@@ -15,12 +15,53 @@ export function shield(
   const refName = name
   return { name, refName, linkUrl, shieldUrl, lang }
 }
-// export const shields = ({ repoUrl }: { repoUrl }): Shield[] => [
-//     shield("Contributors"),
-//     shield("Forks"),
-//     shield("Stargazers"),
-//     shield("Commit activity"),
-//     shield("Branch action runs"),
-//     shield("Issues"),
-//     shield("MIT License"),
-// ]
+const shieldUrl = (path: string, style: string) =>
+  `https://img.shields.io/${path}.svg?style=${style}`
+export const shields = (
+  { repoUrl, githubUsername, repoName, style, commitActivityInterval, branch }:
+    {
+      repoUrl: string
+      repoName: string
+      githubUsername: string
+      style: string
+      commitActivityInterval: "w"
+      branch: string
+    },
+): Shield[] =>
+  [
+    shield(
+      "Contributors",
+      `${repoUrl}/graphs/contributors`,
+      `github/contributors/${githubUsername}/${repoName}`,
+    ),
+    shield(
+      "Forks",
+      `${repoUrl}/network/members`,
+      `github/forks/${githubUsername}/${repoName}`,
+    ),
+    shield(
+      "Stargazers",
+      `${repoUrl}/stargazers`,
+      `github/stars/${githubUsername}/${repoName}`,
+    ),
+    shield(
+      "Commit activity",
+      `${repoUrl}/commits`,
+      `github/commit-activity/${commitActivityInterval}/${githubUsername}/${repoName}`,
+    ),
+    shield(
+      "Branch action runs",
+      `${repoUrl}/actions?query=${encodeURIComponent(`branch:${branch}`)}`,
+      `github/check-runs/${githubUsername}/${repoName}/${branch}`,
+    ),
+    shield(
+      "Issues",
+      `${repoUrl}/issues`,
+      `github/license/${githubUsername}/${repoName}`,
+    ),
+    shield(
+      "License",
+      `${repoUrl}/blob/master/LICENSE.txt`,
+      `github/license/${githubUsername}/${repoName}`,
+    ),
+  ].map((x) => ({ ...x, shieldUrl: shieldUrl(x.shieldUrl, style) }))

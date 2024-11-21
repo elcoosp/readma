3e0dc90611461cc902928f0624dca7e7ef65a138
elcoosp <elcoosp@gmail.com>
2024-11-21 23:24:13 +0100

    fix(core): sync deno conf version

--- cli/mod.test.ts
+++ cli/mod.test.ts
@@ -4,20 +4,6 @@ import { cli } from "./cli.ts"
 Deno.test("detectLanguage", async () => {
   const expected = {
     language: "ts" as const,
-    files: {
-      rs: undefined,
-      ts: {
-        "name": "@readma/cli",
-        "version": "0.0.0",
-        "exports": "./mod.ts",
-        "tasks": {
-          /** Test */
-          "t": "deno test -A",
-          /** Doc */
-          "d": "deno doc --html mod.ts",
-        },
-      },
-    },
     workspaceMembers: undefined,
   }
   const actual = await cli.detectLanguage()

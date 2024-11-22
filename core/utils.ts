c8348d50ad9cf3565b1beafac417e8a394001564
elcoosp <elcoosp@gmail.com>
2024-11-22 17:06:30 +0100

    fix(core): doc & ret ty on readReadmaConfig

--- core/utils.ts
+++ core/utils.ts
@@ -8,7 +8,10 @@ export const md = {
     "```" + lng + `\n${c.trim()}\n` + "```",
 }
 
-export const readReadmaConfig = async (configPathRoot = "./") => {
+/** Create a temp file to log the imported config into the deno runtime, not juste a file read, this is interpreted */
+export const readReadmaConfig = async (
+  configPathRoot = "./",
+): Promise<ReadmeTemplateArgs> => {
   // Can not run if using Deno.makeTempFile
   const tempFilePath = "./readReadmaConfig-tempFile.ts"
   const readmaConfigRelPath = `${configPathRoot}readma.config.ts`

33d17c22b5e9602c04e4b84ac735a1e3c84eef93
elcoosp <elcoosp@gmail.com>
2024-11-26 19:41:24 +0100

    docs(core): processFile

--- core/mdx-manager.ts
+++ core/mdx-manager.ts
@@ -1,7 +1,12 @@
 import { compile, run } from '@mdx-js/mdx'
 import { renderToStaticMarkup } from 'react-dom/server'
 import * as runtime from 'react/jsx-runtime'
-export async function processFile(path: string) {
+/**
+ * Turn a mdx file path into an html string
+ * @param path Path to a mdx file
+ * @returns A rendered html string of react-dom/server or undefined in case of error (except file not found)
+ */
+export async function processFile(path: string): Promise<string | undefined> {
   try {
     const file = await Deno.readTextFile(path)
     console.log(`Rendering ${path}`)

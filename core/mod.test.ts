d3b8f2d51a63a3d73cfab618ba9fa09b4c220e4d
elcoosp <elcoosp@gmail.com>
2024-11-27 12:39:41 +0100

    test: fix core test, do not rely on current README, this should be done in cli test which has whole context

--- core/mod.test.ts
+++ core/mod.test.ts
@@ -1,15 +1,15 @@
 import { assertEquals } from '@std/assert'
 import { readme } from './mod.ts'
 import config from '../readma.config.ts'
+import { assertSnapshot } from '@std/testing/snapshot'
 
-Deno.test('readme', async () => {
-  const current = await Deno.readTextFile('../README.md')
+Deno.test('readme - core', async (t) => {
   if (Deno.env.get('GITHUB_REF')?.startsWith('refs/tags')) {
     assertEquals(true, true)
   } else {
     const out = await readme({ ...config, license: 'MIT' }, {
       folderPath: '../',
     })
-    assertEquals(out, current)
+    await assertSnapshot(t, out)
   }
 })

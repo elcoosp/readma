609d2091339f1703312bada0e95e7e23c9631fdc
elcoosp <elcoosp@gmail.com>
2024-11-27 11:06:52 +0100

    chore: fmt

--- core/shields.test.ts
+++ core/shields.test.ts
@@ -1,40 +1,40 @@
 import { assertSnapshot } from '@std/testing/snapshot'
 import { shields } from './shields.ts'
 const shieldsConfig = {
-    githubUsername: 'elcoosp',
-    repoUrl: 'https://github.com/elcoosp/readma',
-    repoName: 'readma',
-    badgeStyle: 'for-the-badge',
-    commitActivityInterval: 'w',
-    linkedinUsername: undefined,
-    branch: 'main',
-    language: 'ts',
-    vcsName: 'github',
-    workspaceMember: undefined,
-    packageRegistry: 'npm',
+  githubUsername: 'elcoosp',
+  repoUrl: 'https://github.com/elcoosp/readma',
+  repoName: 'readma',
+  badgeStyle: 'for-the-badge',
+  commitActivityInterval: 'w',
+  linkedinUsername: undefined,
+  branch: 'main',
+  language: 'ts',
+  vcsName: 'github',
+  workspaceMember: undefined,
+  packageRegistry: 'npm',
 } as const
 Deno.test('shields - ts', async (t) => {
-    const actual = shields(shieldsConfig)
-    await assertSnapshot(t, actual)
+  const actual = shields(shieldsConfig)
+  await assertSnapshot(t, actual)
 })
 
 Deno.test('shields - rs', async (t) => {
-    const actual = shields({ ...shieldsConfig, language: 'rs' })
-    await assertSnapshot(t, actual)
+  const actual = shields({ ...shieldsConfig, language: 'rs' })
+  await assertSnapshot(t, actual)
 })
 
 Deno.test('shields - linkedinUsername', async (t) => {
-    const actual = shields({ ...shieldsConfig, linkedinUsername: 'test' })
-    await assertSnapshot(t, actual)
+  const actual = shields({ ...shieldsConfig, linkedinUsername: 'test' })
+  await assertSnapshot(t, actual)
 })
 
 Deno.test('shields - packageRegistry', async (t) => {
-    const jsr = shields({ ...shieldsConfig, packageRegistry: 'jsr' })
-    await assertSnapshot(t, jsr)
-    const cratesIo = shields({
-        ...shieldsConfig,
-        packageRegistry: 'crates.io',
-        language: 'rs',
-    })
-    await assertSnapshot(t, cratesIo)
+  const jsr = shields({ ...shieldsConfig, packageRegistry: 'jsr' })
+  await assertSnapshot(t, jsr)
+  const cratesIo = shields({
+    ...shieldsConfig,
+    packageRegistry: 'crates.io',
+    language: 'rs',
+  })
+  await assertSnapshot(t, cratesIo)
 })

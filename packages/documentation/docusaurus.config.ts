606fcbc8d1ba2550ff6ff7031095a11462e64c90
elcoosp <elcoosp@gmail.com>
2024-11-30 00:03:37 +0100

    fix: docs was ignored rename to documentation

---
+++ packages/documentation/docusaurus.config.ts
@@ -0,0 +1,19 @@
+import {
+  classicPreset,
+  createDocusaurusConfig,
+  themeConfig,
+} from '@elcoosp-configs/docusaurus'
+
+const config = createDocusaurusConfig(
+  { organizationName: 'elcoosp', projectName: 'readma' },
+  (derived) => {
+    return ({
+      tagline: 'Beautiful effortless README generator',
+      plugins: [],
+      presets: [classicPreset(derived)],
+      themeConfig: themeConfig(derived),
+    })
+  },
+)
+
+export default config
---
+++ packages/documentation/src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,72 @@
+import Heading from '@theme/Heading'
+import clsx from 'clsx'
+import LayersIcon from '../LayersIcon'
+import styles from './styles.module.css'
+
+type FeatureItem =
+  & {
+    title: string
+    description: JSX.Element
+  }
+  & ({
+    Svg: React.ComponentType<React.ComponentProps<'svg'>>
+  } | { img: string })
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: 'Workspace compatible',
+    Svg: LayersIcon,
+    description: (
+      <>
+        Readma was designed from the ground up to be easily installed and used
+        on a workspace
+      </>
+    ),
+  },
+  {
+    title: 'Automatic shields',
+    img: require('@site/static/img/shields-logo.png').default,
+    description: (
+      <>
+        No more shield badges manual gathering, with sane defaults and links
+      </>
+    ),
+  },
+  {
+    title: 'Cross platform',
+    img: require('@site/static/img/deno-rust-logo.png').default,
+    description: (
+      <>
+        Works on both your <code>deno</code> and <code>rust</code> projects
+      </>
+    ),
+  },
+]
+
+function Feature(props: FeatureItem) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className='text--center'>
+        {'img' in props
+          ? <img src={props.img} className={styles.featureSvg}></img>
+          : <props.Svg className={styles.featureSvg} role='img' />}
+      </div>
+      <div className='text--center padding-horiz--md'>
+        <Heading as='h3'>{props.title}</Heading>
+        <p>{props.description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className='container'>
+        <div className='row'>
+          {FeatureList.map((props, idx) => <Feature key={idx} {...props} />)}
+        </div>
+      </div>
+    </section>
+  )
+}

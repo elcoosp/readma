07f594cb1850a063b295ea45a344bd96167b0dd4
elcoosp <elcoosp@gmail.com>
2024-11-29 10:56:17 +0100

    docs: fix readma.config example & disable deno in documentation folder

--- documentation/docs/readma.config.ts
+++ documentation/docs/readma.config.ts
@@ -2,6 +2,9 @@
 import { type types, utils } from '@readma/core'
 const name = 'readma'
 const author = 'elcoosp'
+const urls = {
+  doc: `https://${name}.vercel.app/`,
+}
 const config: types.ReadmeTemplateArgs = {
   language: 'ts',
   title: 'Readma',
@@ -11,9 +14,14 @@ const config: types.ReadmeTemplateArgs = {
   xHandle: author,
   domain: 'gmail',
   email: author,
+  urls,
   repobeats: '1b12e7ffd13fd2d15772a30e8b0a995976150797',
   images: { screenshot: 'images/screenshot.gif', logo: 'images/logo.png' },
   sections: {
+    features: `
+- Workspace compatible
+- Automatic shields
+- Cross platform`,
     projectDescription: 'Beautiful effortless README generator',
     about: 'Create README that rocks',
     installation: utils.md.code(`deno install @${name}/core`),
@@ -21,26 +29,18 @@ const config: types.ReadmeTemplateArgs = {
 - [Best Readme](https://github.com/othneildrew/Best-README-Template)
 - [Badges 4 README](https://github.com/alexandresanlim/Badges4-README.md-Profile)`,
     gettingStarted: 'See screenshot',
+    usage: `🚧 In construction, refer to the [docs](${urls.doc})`,
     roadmap: `
-- [ ] CLI with config file
-- [ ] Auto defaults from known files like Cargo.toml/{deno,package}.json`,
-    usage: `
-    ${
-      utils.md.code(
-        `
-import { readme } from "@readma/core"
-import config from "./readma.config.ts"
-// Generate a single readme file
-await readme(config, { workspaceRootPath: "./" })`,
-        'ts',
-      )
-    }
-`,
+- [x] CLI with config file
+- [x] Workspace readme generated from known files like \`Cargo.toml\`/\`deno.json\`/\`pnpm-workspace.yml\`
+- [x] Github issue template links`,
+    '⛑️ Support':
+      'Software is still **very early** expect **unexpected breaking changes**',
   },
   template: {
-    bugReport: 'bug-report--from-readme',
-    featRequest: 'feature-request---from-readme',
+    bugReport: 'bug-report.yml',
+    featRequest: 'feature-request.yml',
   },
   backToTop: false,
-} as const
+}  as const
 export default config

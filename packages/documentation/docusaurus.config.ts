10fc55b2ae83e9304615643208dfb7ed1dad3b52
elcoosp <elcoosp@gmail.com>
2025-07-21 13:32:05 +0200

    fix: add docusaurus future config

--- packages/documentation/docusaurus.config.ts
+++ packages/documentation/docusaurus.config.ts
@@ -2,18 +2,39 @@ import {
   classicPreset,
   createDocusaurusConfig,
   themeConfig,
-} from '@elcoosp-configs/docusaurus'
-
+} from "@elcoosp-configs/docusaurus";
+const future = {
+  v4: {
+    removeLegacyPostBuildHeadAttribute: true,
+    useCssCascadeLayers: true,
+  },
+  experimental_faster: {
+    swcJsLoader: true,
+    swcJsMinimizer: true,
+    swcHtmlMinimizer: true,
+    lightningCssMinimizer: true,
+    rspackBundler: true,
+    rspackPersistentCache: true,
+    ssgWorkerThreads: true,
+    mdxCrossCompilerCache: true,
+  },
+  experimental_storage: {
+    type: "localStorage",
+    namespace: true,
+  },
+  experimental_router: "hash",
+} as const;
 const config = createDocusaurusConfig(
-  { organizationName: 'elcoosp', projectName: 'readma' },
+  { organizationName: "elcoosp", projectName: "readma" },
   (derived) => {
-    return ({
-      tagline: 'Beautiful effortless README generator',
+    return {
+      future,
+      tagline: "Beautiful effortless README generator",
       plugins: [],
       presets: [classicPreset(derived)],
       themeConfig: themeConfig(derived),
-    })
+    };
   },
-)
+);
 
-export default config
+export default config;
